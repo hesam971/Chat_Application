f0c1743b784ac1d06975233fee8c539bd8a76030
hesam971 <hesam_botorabi@yahoo.com>
2024-09-11 20:22:40 +0200

    Style ...

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios, { AxiosResponse } from 'axios';
 import { useNavigate } from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 type UserInfo = {
     username: string,
@@ -18,21 +18,21 @@ function Register() {
     const [error, SetError] = useState('')
     const navigate = useNavigate();
 
-    function userInformation(e: React.FormEvent<HTMLFormElement>){
+    function userInformation(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
-        if(!username || !lastname || !email || !password){
+        if (!username || !lastname || !email || !password) {
             SetError('All fields are required')
             clearInformation()
-        }else if(password.length < 8){
-            SetError('password must be at least 8 characters')
+        } else if (password.length < 8) {
+            SetError('Password must be at least 8 characters')
             clearInformation()
-        }else{
-            const fetchUserInfo: UserInfo = {username, lastname, email, password}
+        } else {
+            const fetchUserInfo: UserInfo = { username, lastname, email, password }
             const postData = async () => {
                 try {
                     const response: AxiosResponse = await axios.post('http://localhost:3000/register', fetchUserInfo)
                     console.log(response.data.message)
-                    navigate("/login", {replace: true});
+                    navigate("/login", { replace: true });
                 } catch (error: any) {
                     SetError(error.response.data.message)
                     clearInformation()
@@ -42,40 +42,46 @@ function Register() {
         }
     }
 
-    function clearInformation () {
+    function clearInformation() {
         SetUsername('')
         SetLastname('')
         SetEmail('')
         SetPassowrd('')
     }
 
-
-  return (
-    <>
-        <form onSubmit={userInformation}>
-            <div>
-                {error? error: ''}
+    return (
+        <>
+            <div className="container d-flex justify-content-center align-items-center vh-100">
+                <div className="col-md-6">
+                    <form onSubmit={userInformation} className="border p-4 shadow rounded bg-light">
+                        {error &&
+                            <div className="alert alert-warning alert-dismissible fade show" role="alert">
+                                {error}
+                                <button type="button" className="btn-close" onClick={() => SetError('')} aria-label="Close"></button>
+                            </div>
+                        }
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Username</label>
+                            <input type="text" value={username} onChange={(prev) => SetUsername(prev.target.value)} className="form-control" placeholder='username' />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="lastname" className="form-label">Lastname</label>
+                            <input type="text" value={lastname} onChange={(prev) => SetLastname(prev.target.value)} className="form-control" placeholder='lastname' />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" value={email} onChange={(prev) => SetEmail(prev.target.value)} className="form-control" placeholder='email' />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" value={password} onChange={(prev) => SetPassowrd(prev.target.value)} className="form-control" placeholder='password' />
+                        </div>
+                        <button type="submit" className="btn btn-primary w-100">Register</button>
+                    </form>
+                </div>
             </div>
-        <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" value={username} onChange={(prev) => SetUsername(prev.target.value)} placeholder='username' />
-        </div>
-        <div>
-            <label htmlFor="lastname">Lastname</label>
-            <input type="text" value={lastname} onChange={(prev) => SetLastname(prev.target.value)}  placeholder='lastname' />
-        </div>
-        <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" value={email} onChange={(prev) => SetEmail(prev.target.value)} placeholder='email' />
-        </div>
-        <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" value={password} onChange={(prev) => SetPassowrd(prev.target.value)} placeholder='password' />
-        </div>
-        <button type="submit">Register</button>  
-        </form>
-    </>
-  )
+        </>
+    )
 }
 
-export default Register
+export default Register;
